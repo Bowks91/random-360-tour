fd528901b59dfe51166ed344715265381cdda449
Alex <alexander.bowkley@durham.ac.uk>
2023-02-20 15:32:10 +0000

    4
    4

---
+++ data.js
@@ -0,0 +1,391 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-page-1",
+      "name": "Page 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.3002247971062264,
+        "pitch": -0.10945491984031719,
+        "fov": 1.259000478605961
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5499739417471705,
+          "pitch": -0.33010401415836554,
+          "rotation": 7.853981633974483,
+          "target": "1-page-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3331853542588998,
+          "pitch": -0.04567286810838844,
+          "title": "Skyscraper",
+          "text": "Where business people do business"
+        },
+        {
+          "yaw": 1.887772414740505,
+          "pitch": 0.7419260014449875,
+          "title": "River",
+          "text": "Often wet"
+        },
+        {
+          "yaw": 0.6200842984918005,
+          "pitch": 0.20781346068528705,
+          "title": "Houses",
+          "text": "Where people live"
+        }
+      ]
+    },
+    {
+      "id": "1-page-2",
+      "name": "Page 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -1.8668774484281183,
+        "pitch": -0.6175924202063712,
+        "fov": 1.259000478605961
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7689173160394223,
+          "pitch": -0.7395735378249721,
+          "rotation": 1.5707963267948966,
+          "target": "1-page-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.438112507941238,
+          "pitch": -0.7216926241733645,
+          "title": "Time Square",
+          "text": "A good place to find out the time"
+        }
+      ]
+    },
+    {
+      "id": "2-page-3",
+      "name": "Page 3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 1.8289557231059232,
+        "pitch": -0.2482801492144464,
+        "fov": 1.259000478605961
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7534967930646737,
+          "pitch": -0.4269721145095353,
+          "rotation": 1.5707963267948966,
+          "target": "3-page-4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1211457716120634,
+          "pitch": -0.05018995295712614,
+          "title": "Hard Rock Cafe",
+          "text": "A place for expensive t-shirts"
+        }
+      ]
+    },
+    {
+      "id": "3-page-4",
+      "name": "Page 4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1741,
+      "initialViewParameters": {
+        "yaw": -2.4033227920722933,
+        "pitch": -0.03289031900119532,
+        "fov": 1.4223619823453562
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8537724083190295,
+          "pitch": -0.3861486834840875,
+          "rotation": 1.5707963267948966,
+          "target": "4-page-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3655692798824433,
+          "pitch": 0.16280348229089725,
+          "title": "Town",
+          "text": "Absolutely no idea where this is"
+        }
+      ]
+    },
+    {
+      "id": "4-page-5",
+      "name": "Page 5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -3.128509963994814,
+        "pitch": -0.10223991896696383,
+        "fov": 1.4223619823453562
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1054286404908495,
+          "pitch": -0.4228926327028155,
+          "rotation": 1.5707963267948966,
+          "target": "5-page-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.134790601479448,
+          "pitch": 0.47144283747606863,
+          "title": "Dirt path",
+          "text": "Looks bumpy"
+        }
+      ]
+    },
+    {
+      "id": "5-page-6",
+      "name": "Page 6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -0.08800548399420549,
+        "pitch": 0.044479290927313286,
+        "fov": 1.4223619823453562
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.088268835296324,
+          "pitch": -0.34654079922938763,
+          "rotation": 1.5707963267948966,
+          "target": "6-page-7"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.08800548399420549,
+          "pitch": 0.044479290927313286,
+          "title": "Golden Gate Bridge",
+          "text": "Surprisingly red"
+        }
+      ]
+    },
+    {
+      "id": "6-page-7",
+      "name": "Page 7",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.5366210970474263,
+        "pitch": 0.016605300017872082,
+        "fov": 1.4223619823453562
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4753862815785155,
+          "pitch": -0.36290426433563105,
+          "rotation": 1.5707963267948966,
+          "target": "7-page-8"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5833094096058566,
+          "pitch": 0.12265957306810549,
+          "title": "Apartment Block",
+          "text": "Good luck with sea level rise"
+        }
+      ]
+    },
+    {
+      "id": "7-page-8",
+      "name": "Page 8",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1571,
+      "initialViewParameters": {
+        "yaw": -0.6441080699693309,
+        "pitch": -0.009178606147456136,
+        "fov": 1.4223619823453562
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6590589414415788,
+          "pitch": -0.379569007532254,
+          "rotation": 1.5707963267948966,
+          "target": "0-page-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.21951135888293827,
+          "pitch": 0.028757232822362866,
+          "title": "Riverwalk",
+          "text": "Could be Britain, until you notice the trees in the distance"
+        }
+      ]
+    }
+  ],
+  "name": "Random 360 Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};

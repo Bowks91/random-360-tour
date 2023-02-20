c43a8e57ab0f7a50dad564f695b84eb1b36ff1b4
Alex <alexander.bowkley@durham.ac.uk>
2023-02-20 15:16:56 +0000

    Marzipano 360 Tour
    Marzipano 360 Tour

---
+++ data.js
@@ -0,0 +1,391 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-5481b00-natural-landscape-360-aerial",
+      "name": "5481b00-natural-landscape-360-aerial",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 1.32638628945889,
+        "pitch": -0.052471932908392915,
+        "fov": 0.7953370090923572
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6523549438280511,
+          "pitch": -0.3153168714171386,
+          "rotation": 7.853981633974483,
+          "target": "1-dp1j1wey-times-square-360"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.3331853542588998,
+          "pitch": -0.04567286810838844,
+          "title": "Skyscraper",
+          "text": "Where business people do business"
+        },
+        {
+          "yaw": 1.887772414740505,
+          "pitch": 0.7419260014449875,
+          "title": "River",
+          "text": "Often wet"
+        },
+        {
+          "yaw": 0.6200842984918005,
+          "pitch": 0.20781346068528705,
+          "title": "Houses",
+          "text": "Where people live"
+        }
+      ]
+    },
+    {
+      "id": "1-dp1j1wey-times-square-360",
+      "name": "dp1j1wey-times-square-360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -1.9084283206156378,
+        "pitch": -0.3661854262998663,
+        "fov": 1.4223619823453562
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0803444266165094,
+          "pitch": -0.5421379245751332,
+          "rotation": 1.5707963267948966,
+          "target": "2-j8j1nuo-360-image-marina"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.438112507941238,
+          "pitch": -0.7216926241733645,
+          "title": "Time Square",
+          "text": "A good place to find out the time"
+        }
+      ]
+    },
+    {
+      "id": "2-j8j1nuo-360-image-marina",
+      "name": "j8j1nuo-360-image-marina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": 1.8289557231059232,
+        "pitch": -0.2482801492144464,
+        "fov": 1.4223619823453562
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7192031143047544,
+          "pitch": -0.49059285583754075,
+          "rotation": 1.5707963267948966,
+          "target": "3-n5f0lia-360-photo-panorama-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.1211457716120634,
+          "pitch": -0.05018995295712614,
+          "title": "Hard Rock Cafe",
+          "text": "A place for expensive t-shirts"
+        }
+      ]
+    },
+    {
+      "id": "3-n5f0lia-360-photo-panorama-",
+      "name": "n5f0lia-360-photo-panorama-",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1741,
+      "initialViewParameters": {
+        "yaw": -2.4033227920722933,
+        "pitch": -0.03289031900119532,
+        "fov": 1.4223619823453562
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.8537724083190295,
+          "pitch": -0.3861486834840875,
+          "rotation": 1.5707963267948966,
+          "target": "4-sb0criu-360-panorama-shore"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.3655692798824433,
+          "pitch": 0.16280348229089725,
+          "title": "Town",
+          "text": "Absolutely no idea where this is"
+        }
+      ]
+    },
+    {
+      "id": "4-sb0criu-360-panorama-shore",
+      "name": "sb0criu-360-panorama-shore",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -3.128509963994814,
+        "pitch": -0.10223991896696383,
+        "fov": 1.4223619823453562
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.1054286404908495,
+          "pitch": -0.4228926327028155,
+          "rotation": 1.5707963267948966,
+          "target": "5-golden-gate-bridge-360-experience-de5ykhbw"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -3.134790601479448,
+          "pitch": 0.47144283747606863,
+          "title": "Dirt path",
+          "text": "Looks bumpy"
+        }
+      ]
+    },
+    {
+      "id": "5-golden-gate-bridge-360-experience-de5ykhbw",
+      "name": "golden-gate-bridge-360-experience-de5ykhbw",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1824,
+      "initialViewParameters": {
+        "yaw": -0.08800548399420549,
+        "pitch": 0.044479290927313286,
+        "fov": 1.4223619823453562
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.088268835296324,
+          "pitch": -0.34654079922938763,
+          "rotation": 1.5707963267948966,
+          "target": "6-mm01bmi-bal-harbour-florida-360-view"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.08800548399420549,
+          "pitch": 0.044479290927313286,
+          "title": "Golden Gate Bridge",
+          "text": "Surprisingly red"
+        }
+      ]
+    },
+    {
+      "id": "6-mm01bmi-bal-harbour-florida-360-view",
+      "name": "mm01bmi-bal-harbour-florida-360-view",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.5366210970474263,
+        "pitch": 0.016605300017872082,
+        "fov": 1.4223619823453562
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.4753862815785155,
+          "pitch": -0.36290426433563105,
+          "rotation": 1.5707963267948966,
+          "target": "7-jdi9wd3-bridge-360-panorama-virtual-tour"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.5833094096058566,
+          "pitch": 0.12265957306810549,
+          "title": "Apartment Block",
+          "text": "Good luck with sea level rise"
+        }
+      ]
+    },
+    {
+      "id": "7-jdi9wd3-bridge-360-panorama-virtual-tour",
+      "name": "jdi9wd3-bridge-360-panorama-virtual-tour",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1571,
+      "initialViewParameters": {
+        "yaw": -0.6441080699693309,
+        "pitch": -0.009178606147456136,
+        "fov": 1.4223619823453562
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6590589414415788,
+          "pitch": -0.379569007532254,
+          "rotation": 1.5707963267948966,
+          "target": "0-5481b00-natural-landscape-360-aerial"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.21951135888293827,
+          "pitch": 0.028757232822362866,
+          "title": "Riverwalk",
+          "text": "Could be Britain, until you notice the trees in the distance"
+        }
+      ]
+    }
+  ],
+  "name": "Random 360 Tour",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
